b890882301f88b4570fe45f7c1f18d0df71ba7a7
Matt <matt23488@gmail.com>
2016-10-13 15:46:12 -0400

    Forgot to move all my event handlers from the old New Record page

--- OfficeOlympicsWeb/Scripts/Home/Index/events.js
+++ OfficeOlympicsWeb/Scripts/Home/Index/events.js
@@ -63,4 +63,67 @@
     $("#newRecordModal").on("hidden.bs.modal", function () {
         $("#newRecordForm").clearForm();
     });
+
+    $("#newRecordForm #Record_Competitor_CompetitorId").on("change", function () {
+        var $competitor = $(this);
+        var $witness1 = $("#newRecordForm #Record_Witness1_CompetitorId");
+        var $witness2 = $("#newRecordForm #Record_Witness2_CompetitorId");
+
+        var competitorId = $(":selected", $competitor).val().trim();
+        var witness1Id = $(":selected", $witness1).val().trim();
+        var witness2Id = $(":selected", $witness2).val().trim();
+
+        // If default option is selected, do nothing
+        if (competitorId.length === 0) return;
+
+        // If conflicting options are selected, revert the others back to the default option
+        if (witness1Id === competitorId) {
+            $("option:eq(0)", $witness1).prop("selected", true);
+        }
+        if (witness2Id === competitorId) {
+            $("option:eq(0)", $witness2).prop("selected", true);
+        }
+    });
+
+    $("#newRecordForm #Record_Witness1_CompetitorId").on("change", function () {
+        var $competitor = $("#newRecordForm #Record_Competitor_CompetitorId");
+        var $witness1 = $(this);
+        var $witness2 = $("#newRecordForm #Record_Witness2_CompetitorId");
+
+        var competitorId = $(":selected", $competitor).val().trim();
+        var witness1Id = $(":selected", $witness1).val().trim();
+        var witness2Id = $(":selected", $witness2).val().trim();
+
+        // If default option is selected, do nothing
+        if (witness1Id.length === 0) return;
+
+        // If conflicting options are selected, revert the others back to the default option
+        if (competitorId === witness1Id) {
+            $("option:eq(0)", $competitor).prop("selected", true);
+        }
+        if (witness2Id === witness1Id) {
+            $("option:eq(0)", $witness2).prop("selected", true);
+        }
+    });
+
+    $("#newRecordForm #Record_Witness2_CompetitorId").on("change", function () {
+        var $competitor = $("#newRecordForm #Record_Competitor_CompetitorId");
+        var $witness1 = $("#newRecordForm #Record_Witness1_CompetitorId");
+        var $witness2 = $(this);
+
+        var competitorId = $(":selected", $competitor).val().trim();
+        var witness1Id = $(":selected", $witness1).val().trim();
+        var witness2Id = $(":selected", $witness2).val().trim();
+
+        // If default option is selected, do nothing
+        if (witness2Id.length === 0) return;
+
+        // If conflicting options are selected, revert the others back to the default option
+        if (competitorId === witness2Id) {
+            $("option:eq(0)", $competitor).prop("selected", true);
+        }
+        if (witness1Id === witness2Id) {
+            $("option:eq(0)", $witness1).prop("selected", true);
+        }
+    });
 });
